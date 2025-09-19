32732d42b2b8d9863a7a3ca4d4e6b5367026b53f
sharonwangui3991-pixel <sharonwangui3991@gmail.com>
2025-09-19 17:29:23 +0300

    Add files via upload

---
+++ 2.cxx
@@ -0,0 +1,21 @@
+//simple c program ( "Hello c"),
+/*
+Name:sharon	
+Reg:26218
+Description:"Hello c";
+*/
+
+#include<stdio.h>//pre-processor directive 
+
+// main fuction
+int main( ) {
+printf("1.SPH 2107 Analogue Electronics\n");
+printf("2.SPI 2101 Introduction to programming and Algorithms\n"); 
+printf("3.SPI 2102 Computer systems and organisations\n");   
+printf("4.SPI 2103 Introduction to computer Applications\n"); 
+printf("5.SPI2120 Essentials of Accounting\n");  
+printf("6.SPM 2104 Mathematics for science\n"); 
+printf("7.UCU 2105 Fundamentals of ICT\n");
+printf("8.UCU 2106 Communications and Information literacy\n"); 
+return 0;
+}
