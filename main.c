23fb384aedf24f0838c092dd75a8774d97afbdcb
sharonwangui3991-pixel <sharonwangui3991@gmail.com>
2025-09-24 16:03:21 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-//simple c program
+//"simple c program";
 /*
 Name:sharon wangui njoki
 Reg:CT100/G/26218/25
@@ -7,19 +7,17 @@ Description:program for a loan
 #include <stdio.h>
 
 int main() {
-    int age;
-    int income;
-    printf("Enter age");
-    scanf("%d",&age);
-    printf("Enter income");
-    scanf("%d",&income);
-    if(age>=21 && income>=21000)
-    {
-        printf("congratulations you qualify for a loan");
-    }
-    else
-            {
-                printf("unfortunately we are unable to offer you a loan at this time");
-            }
+    double radius;
+    double height;
+    double pi=3.14;
+    printf("enter radius");
+    scanf("%lf",&radius);
+    
+    printf("enter height");
+    scanf("%lf",&height);
+    double volume=pi*radius*radius*height;
+    double surfacearea=2*pi*radius*radius+2*pi*radius*height;
+    printf("%lf\n",volume);
+    printf("%lf",surfacearea);
     return 0;
 }
