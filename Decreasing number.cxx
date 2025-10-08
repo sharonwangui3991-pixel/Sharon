0d7fdbc37b35ec35c793e86f315a48e5cafa935c
sharonwangui3991-pixel <sharonwangui3991@gmail.com>
2025-10-08 22:09:30 +0300

    Add files via upload

---
+++ Decreasing number.cxx
@@ -0,0 +1,17 @@
+//"simple c program";
+/*
+Name:sharon wangui njoki
+Reg:CT100/G/26218/25
+Description:program for a decreasing number
+*/
+
+#include <stdio.h>
+
+int main() {
+    int i;
+    for (i=100;i>=50;i--){
+        printf("%d\n",i);
+    }
+    return 0;
+}
+	
