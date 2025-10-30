6728b131dd021b98e8a4c637ee214325859a1b1c
sharonwangui3991-pixel <sharonwangui3991@gmail.com>
2025-10-30 13:36:01 +0300

    Add files via upload

---
+++ fare.cxx
@@ -0,0 +1,11 @@
+//Simple c program
+/*
+Name: Sharon wangui Njoki	
+Reg: CT100/G/26218/25
+Description:calculation of fare
+*/
+#include <stdio.h>   
+//pre-processor descriptive.scanf
+
+//main fuction
+int main()
